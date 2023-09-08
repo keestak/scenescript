c510721764ddac2a3f7dc822c538d821abaaa7f5
keestak <keestak@proton.me>
2023-09-08 07:24:07 -0400

    god dammit

--- tokenizer.gd
+++ tokenizer.gd
@@ -58,7 +58,7 @@ func scan() -> scenescript_token:
 		return number()
 	
 	if c.is_valid_identifier():
-		if not (c == '_' and peek(0) in [' ', '\t', '\n']): #special case for single underscore
+		if not (c == '_' and not (peek(0).is_valid_identifier() or peek(0).is_valid_int()) ):#in [' ', '\t', '\n']): #special case for single underscore
 			return identifier_or_keyword()
 	
 	match c:
